303bd4d84a6f0dfbff7d40d3266adcc87cc81e0e
coderemon24 <mdemonahmed2021@gmail.com>
2025-07-19 12:02:22 +0600

    All endpoints created for users

--- internal/api/rest/handlers/userHandler.go
+++ internal/api/rest/handlers/userHandler.go
@@ -18,17 +18,67 @@ func UserRoutes(rh *rest.HttpHandler) {
 	handler := UserHandler{}
 	
 	// public endpoints
-	app.Get("/users", handler.UserList)
-	
+	app.Post("/users/login", handler.UserLogin)
+	app.Post("/users/register", handler.UserRegister)
 	
 	
 	// private endpoints
+	app.Get("/users", handler.UserList)
+	app.Post("/users", handler.UserCreate)
+	app.Put("/users/:id", handler.UserUpdate)
+	app.Delete("/users/:id", handler.UserDelete)
+	app.Get("/users/:id", handler.UserById)
+	app.Get("/users/email/:email", handler.UserByEmail)
 }
 
 
-// receiver function
+// receiver functions -> methods
+
+// user list
 func (h *UserHandler) UserList(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message" : "user list",
+	})
+}
+// user by id
+func (h *UserHandler) UserById(ctx *fiber.Ctx) error{
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "user by id",
+	})
+}
+// user by email
+func (h *UserHandler) UserByEmail(ctx *fiber.Ctx) error {
+	return ctx.Status(200).JSON(&fiber.Map{
+		"message": "user by email",
+	})
+}
+// user create
+func (h *UserHandler) UserCreate(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "user created",
+	})
+}
+// user update
+func(h *UserHandler)UserUpdate(ctx *fiber.Ctx)error{
+	return ctx.Status(200).JSON(&fiber.Map{
+		"message": "user updated",
+	})
+}
+// user delete
+func(h *UserHandler) UserDelete(ctx *fiber.Ctx) error {
+	return ctx.Status(200).JSON(&fiber.Map{
+		"message" : "user deleted",
+	})
+}
+// user login
+func (h *UserHandler) UserLogin(ctx *fiber.Ctx) error {
+	return ctx.Status(200).JSON(&fiber.Map{
+		"message": "user logged in",
+	})
+}
+// user register
+func (h *UserHandler) UserRegister(ctx *fiber.Ctx) error {
+	return ctx.Status(200).JSON(&fiber.Map{
+		"message": "user registered",
 	})
 }
